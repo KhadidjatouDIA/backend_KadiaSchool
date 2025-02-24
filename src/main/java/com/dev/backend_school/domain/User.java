ec3467c93fac61bf477b79e5fca620fd15494934
KhadidjatouDIA <sokhnakhadidjah@gmail.com>
2025-02-24 09:59:24 +0000

    Add Security

---
+++ src/main/java/com/dev/backend_school/domain/User.java
@@ -0,0 +1,14 @@
+package com.dev.backend_school.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    String firstName;
+    String lastName;
+    String email;
+}
